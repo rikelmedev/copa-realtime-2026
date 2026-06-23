c97c7cb90d552052230da1c884a45c4c9fb1e417
Rikelme <rikelmemini100@gmail.com>
2026-06-23 15:34:23 -0300

    feat: register data routes in api

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const eventsRouter = require('./routes/events');
+const dataRouter = require('./routes/data');
 const { connect } = require('./config/rabbitmq');
 const { startPoller } = require('./jobs/poller');
 const { startConsumer } = require('./jobs/consumer');
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/events', eventsRouter);
+app.use('/api', dataRouter);
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok', service: 'copa-api' });
